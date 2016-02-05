2212e8a85786cec22a85d51d4a03f32ddfe12411
tjorriemorrie <jacoj82@gmail.com>
2016-02-05 18:21:18 +0200

    therapies skeleton done

--- assets/jsx/Therapies.jsx
+++ assets/jsx/Therapies.jsx
@@ -7,18 +7,44 @@ export default class Therapies extends React.Component {
         console.info('[Therapies] render');
         return <div>
 
-            <h2 className="heading">Therapies</h2>
+            <div className="banner">
+                <div className="wrapper">
+                    <img src="/static/img/logo.png"/>
+                    <h1>Therapies</h1>
+                </div>
+            </div>
 
-            A wide variety of safe, natural and non-invasive therapies are offered to meet all your healthcare needs.
--	Lifestyle counseling
-
-            -	Iridology
--	Diet & applied nutrition
--	Herbal medicine and supplementation
--	Swedish massage
--	Detox
--	Hydrotherapy
--	Behavioral/cognitive therapy
+            <div className="therapies">
+                <div className="wrapper">
+                    <p>
+                        <img src="/static/img/the_eyes.jpg"/>
+                    </p>
+                    <dl>
+                        <dt>Therapies</dt>
+                        <dd>Iridology / Iris diagnosis</dd>
+                        <dd>Herbal medicine / Supplementation</dd>
+                        <dd>Nutritional therapy</dd>
+                        <dd>Hydrotherapy / Detoxification</dd>
+                        <dd>Behavioural / Cognitive Therapy</dd>
+                        <dd>Excersice Advice</dd>
+                        <dd>Lifestyle councelling</dd>
+                        <dd>Swedish Massage</dd>
+                    </dl>
+                    <p>
+                        <img src="/static/img/the_pills.jpg"/>
+                    </p>
+                    <div className="testimonies">
+                        <h3>Testimonies</h3>
+                        <blockquote>
+                            <p>"I used to struggle with migraines. Since changing my diet and lifestyle I am pain free."</p>
+                            <small>~ Susan Allan</small>
+                        </blockquote>
+                    </div>
+                    <p>
+                        <img src="/static/img/the_mass.jpg"/>
+                    </p>
+                </div>
+            </div>
 
         </div>;
     }
