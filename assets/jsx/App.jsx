aac0486a5a9869ead8f00babecc31ff77c897dce
tjorriemorrie <jacoj82@gmail.com>
2016-06-14 18:38:17 +0200

    #11 added newsletter signup, closes #11

--- assets/jsx/App.jsx
+++ assets/jsx/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import Navigation from './Navigation.jsx';
 import Footer from './Footer.jsx';
+import Signup from './Signup.jsx';
 require("normalize-css");
 require("./../less/main.less");
 
@@ -10,6 +12,7 @@ export default class App extends React.Component {
     render() {
         console.info('[App] render');
         return <div>
+            <Signup/>
             <Navigation/>
             {React.cloneElement(this.props.children, {})}
             <Footer/>
