ffd856a68ef39b13e9ee703a6c62024549b70038
tjorriemorrie <jacoj82@gmail.com>
2016-06-14 15:12:21 +0200

    #8 added facebook social page plugin to footer

--- assets/jsx/Footer.jsx
+++ assets/jsx/Footer.jsx
@@ -3,32 +3,48 @@ import React from 'react';
 
 export default class Footer extends React.Component {
 
+    componentDidMount() {
+        console.info('[Footer] mounted');
+        document.addEventListener('fb_init', e => FB.XFBML.parse());
+    }
+
     render() {
         console.info('[Footer] render');
         return <footer>
             <div className="wrapper">
                 <div className="boxes">
+
+                    <div className="fb-page"
+                         data-href="https://www.facebook.com/besetfreelifestyle/"
+                         data-tabs="timeline"
+                         data-width="500"
+                         data-height="400"
+                         data-small-header="true"
+                         data-adapt-container-width="false"
+                         data-hide-cover="false"
+                         data-show-facepile="false">
+                        <blockquote cite="https://www.facebook.com/besetfreelifestyle/"
+                                    className="fb-xfbml-parse-ignore">
+                            <a href="https://www.facebook.com/besetfreelifestyle/">Be Set Free</a>
+                        </blockquote>
+                    </div>
+
                     <div>
                         <p>
                             Tel: 061 914 2177
                             <br/>Practice at Centurion and Fourways
                             <br/><a href="mailto:info@besetfree.co.za">info@besetfree.co.za</a>
                         </p>
-                    </div>
-                    <div>
+                        <hr/>
                         <h3>Nerine Jansen</h3>
                         <p>Natural healing practitioner</p>
                         <p>PRO 08329 T</p>
                     </div>
-                    <div>
-                        <p>For weekly information on
-                            <br/>nutrition and other health
-                            <br/>tips follow my <a href="https://www.facebook.com/besetfreelifestyle/">facebook page</a>
-                        </p>
-                    </div>
                 </div>
 
-                <p className="copy"><small>&copy; 2016 by Nerine Jansen.</small></p>
+                <p className="copy">
+                    <small>&copy; 2016 by Nerine Jansen.</small>
+                </p>
             </div>
         </footer>;
     }
--- gae/src/views.py
+++ gae/src/views.py
@@ -7,22 +7,22 @@ from google.appengine.api import mail
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/contact/email', methods=['POST'])
 @jsonapi
 def contact_email():
-	app.logger.info('Contact email: {}'.format(request.form))
-	mail.send_mail_to_admins(
-		sender='{}'.format('jacoj82@gmail.com'),
-		subject='Enquiry',
-		body='{}\n\nName: {}\nNumber: {}\nEmail: {}'.format(
-			request.form.get('enquiry', 'enquiry empty'),
-			request.form.get('name', 'noname'),
-			request.form.get('number', 'nonumber'),
-			request.form.get('email', 'noemail'),
-		),
-	)
+    app.logger.info('Contact email: {}'.format(request.form))
+    mail.send_mail_to_admins(
+        sender='{}'.format('jacoj82@gmail.com'),
+        subject='Enquiry',
+        body='{}\n\nName: {}\nNumber: {}\nEmail: {}'.format(
+            request.form.get('enquiry', 'enquiry empty'),
+            request.form.get('name', 'noname'),
+            request.form.get('number', 'nonumber'),
+            request.form.get('email', 'noemail'),
+        ),
+    )
 
-	return
+    return
