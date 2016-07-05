a7b8a29670694a8571cf32e5428bda9726519944
tjorriemorrie <jacoj82@gmail.com>
2016-07-05 15:22:32 +0200

    About dynamic routing set up

--- assets/jsx/routes/routes/about/index.js
+++ assets/jsx/routes/routes/about/index.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import {Route, IndexRoute} from 'react-router'
-
-
-const loadAbout = (nextState, callback) => {
-    require.ensure([], (require) => {
-        callback(null, require('./components/about').default)
-    })
+const route = {
+    path: 'about',
+    getComponent(nextState, callback) {
+        require.ensure([], (require) => {
+            callback(null, require('./components/about').default)
+        })
+    }
 }
-
-const route = <Route path="/about" getComponent={loadAbout} />
 
 export default route
